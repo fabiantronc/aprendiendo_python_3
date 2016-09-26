894615c3496d0a45f67f75320a073383bd01c4b0
Fabiantronc <Fabiantronc@gmail.com>
2016-09-26 01:46:08 -0300

    Se mejoró archivo

---
+++ calculo_radio_mejorado.py
@@ -0,0 +1,10 @@
+# Calculo radio
+# Creo una variable con el valor de PI
+pi = 3.14158
+# Le pedimos datos al usuario
+respuesta = float(raw_input('Cual es su radio '))
+# Transformamos esos datos a valores flotantes
+#respuesta_validada = float(respuesta)
+#Realizamos la operacion matematica
+area = pi * respuesta**2
+print'El area es', area
